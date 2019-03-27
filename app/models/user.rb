96a128431058b87f1cb74940cf577b7298030689
clairemuller <clairermuller@gmail.com>
2019-03-27 12:50:25 -0700

    finished setting up matches

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :dislikes, dependent: :destroy
-  has_many :matches, through: :likes, dependent: :destroy
   has_many :inverse_likes, class_name: "Like", foreign_key: "likee_id", dependent: :destroy
+  has_many :inverse_dislikes, class_name: "Dislike", foreign_key: "dislikee_id", dependent: :destroy
+  has_many :matches, through: :likes, dependent: :destroy
 
   # makes sure all fields are filled
   validates :username, :email, :location, :pet_name, :pet_age, :pet_bio, :pet_pic, presence: true
@@ -21,26 +22,46 @@ class User < ApplicationRecord
 
   has_one_attached :pet_pic
 
-  # trying to validate that pic is an image, doesn't work:
-  # validates :pet_pic, { with: "image/jpg", "image/jpeg", "image/png", "image/gif"}
+  # HELPER METHODS
 
-  # get all users that haven't been liked or disliked by user
+  # returns array - all users that haven't been liked or disliked by user
   def users_not_seen_yet
     arr = []
-    arr << self.get_likee_ids << self.get_dislikee_ids << self.id
+    arr << self.who_user_likes << self.who_user_dislikes << self.id
     arr = arr.flatten
     User.where.not(id: arr)
   end
 
-  def get_likee_ids
+  def who_user_likes
     self.likes.map do |like|
       like.likee_id
     end
   end
 
-  def get_dislikee_ids
+  def who_user_dislikes
     self.dislikes.map do |dislike|
       dislike.dislikee_id
+    end
+  end
+
+  def who_likes_user
+    self.inverse_likes.map do |inv_like|
+      inv_like.user_id
+    end
+  end
+
+  def get_match_ids
+    self.who_likes_user.select do |liker|
+      self.who_user_likes.select do |like|
+        like == liker
+      end
+    end
+  end
+
+  def get_matches
+    match_ids = self.get_match_ids
+    match_ids.map do |match_id|
+      User.find_by(id: match_id)
     end
   end
 
