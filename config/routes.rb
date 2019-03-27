9150c7db7358561938157b18eea0c8c0a7c885f1
clairemuller <clairermuller@gmail.com>
2019-03-26 17:57:57 -0700

    working on creating matches

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :matches
   resources :likes
   resources :users
 
   get '/users/:id/random', to: 'users#random'
-  
+  get '/users/:id/likes', to: 'users#likes'
 
 end
