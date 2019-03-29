2f922010d9f69fc423aa41c7a502e011153e663e
github username <github email address>
2019-03-29 12:36:18 -0700

    timezone

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,8 +3,12 @@ class Message < ApplicationRecord
   belongs_to :user
   validates_presence_of :body, :conversation_id, :user_id
 
+  def to_pst
+    in_time_zone('Pacific Time (US & Canada)')
+  end
+
   def message_time
-    created_at.strftime("%m/%d/%y at %l:%M %p")
+    created_at.in_time_zone("Pacific Time (US & Canada)").strftime("%m/%d/%y at %l:%M %p")
   end
 
 end
